70beca57e77bcdda2c977c6842b1c488fd81e902
Brett Okken <brett.okken@oracle.com>
2026-01-31 12:53:07 -0600

    convert iso8859x test to junit add encoder charbuffer parameters

--- test/jdk/sun/nio/cs/ISO8859x.java
+++ test/jdk/sun/nio/cs/ISO8859x.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,22 +21,41 @@
  * questions.
  */
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /*
  * @test
  * @bug 6419791
  * @summary
+ * @run junit ISO8859x
  * @author Martin Buchholz
  */
-
-import java.io.*;
-import java.util.*;
-import java.nio.charset.*;
-import java.nio.*;
-
 public class ISO8859x {
-    final static byte[] lowBytes = new byte[0xa0];
-    final static char[] lowChars = new char[0xa0];
-    final static String lowString;
+    private static final byte[] lowBytes = new byte[0xa0];
+    private static final char[] lowChars = new char[0xa0];
+    private static final String lowString;
+    private static final List<Charset> charsets = Charset.availableCharsets().values().stream()
+                                                         .filter(cs -> cs.name().matches(".*(8859).*"))
+                                                         .toList();
+
     static {
         for (int i = 0; i < 0xa0; i++) {
             lowBytes[i] = (byte) i;
@@ -45,14 +64,63 @@ public class ISO8859x {
         lowString = new String(lowChars);
     }
 
-    private static void testCharset(Charset cs) throws Throwable {
+    private static List<Charset> charsets() {
+        return charsets;
+    }
+
+    private static List<Arguments> encodeArguments() {
+        List<Arguments> args = new ArrayList<>(charsets.size() * 5);
+        for (Charset c : charsets) {
+            String name = c.name();
+            Function<String, CharBuffer> cbFx = CharBuffer::wrap;
+            args.add(Arguments.of(c, cbFx, name + " wrapped String"));
+            cbFx = s -> CharBuffer.wrap(s.toCharArray());
+            args.add(Arguments.of(c, cbFx, name + " wrapped char[]"));
+            cbFx = s -> CharBuffer.wrap(s.toCharArray()).asReadOnlyBuffer();
+            args.add(Arguments.of(c, cbFx, name + " read only wrapped char[]"));
+            cbFx = s -> CharBuffer.wrap(CharBuffer.wrap(s));
+            args.add(Arguments.of(c, cbFx, name + " wrapped wrapped String"));
+        }
+        return args;
+    }
+
+    @ParameterizedTest
+    @MethodSource("charsets")
+    void testCanEncode(Charset cs) {
+        assertTrue(cs.canEncode());
+    }
+
+    @ParameterizedTest
+    @MethodSource("charsets")
+    void testGetBytes(Charset cs) throws Exception {
         String csn = cs.name();
-        System.out.println(csn);
+        assertArrayEquals(lowBytes, lowString.getBytes(csn));
+    }
 
-        check(cs.canEncode());
-        check(Arrays.equals(lowString.getBytes(csn), lowBytes));
-        check(new String(lowBytes, csn).equals(lowString));
+    @ParameterizedTest
+    @MethodSource("charsets")
+    void testNewString(Charset cs) throws Exception {
+        String csn = cs.name();
+        assertEquals(lowString, new String(lowBytes, csn));
+    }
 
+    @ParameterizedTest(name="{2}")
+    @MethodSource("encodeArguments")
+    void testEncodeAscii(Charset cs, Function<String, CharBuffer> cbFunction, String name) {
+        CharsetEncoder encoder = cs.newEncoder();
+        encoder.onUnmappableCharacter(CodingErrorAction.REPORT)
+               .onMalformedInput(CodingErrorAction.REPORT);
+        ByteBuffer target = ByteBuffer.allocate(lowBytes.length);
+        CharBuffer cb = cbFunction.apply(lowString);
+        CoderResult result = encoder.encode(cb, target, true);
+        assertEquals(CoderResult.UNDERFLOW, result);
+        assertFalse(cb.hasRemaining());
+        assertArrayEquals(lowBytes, target.array());
+    }
+
+    @ParameterizedTest(name="{2}")
+    @MethodSource("encodeArguments")
+    void testEncoderDecoder(Charset cs, Function<String, CharBuffer> cbFunction, String name) throws Exception {
         CharsetEncoder encoder = cs.newEncoder();
         CharsetDecoder decoder = cs.newDecoder();
         decoder.onUnmappableCharacter(CodingErrorAction.REPORT)
@@ -64,42 +132,12 @@ public class ISO8859x {
         for (int c = 0xa0; c < 0x100; c++) {
             try {
                 bytes[0] = (byte) c;
-                char[] chars;
-                try { chars = decoder.decode(ByteBuffer.wrap(bytes)).array(); }
-                catch (UnmappableCharacterException x) { continue; }
-                equal(chars.length, 1);
-                byte[] bytes2 = encoder.encode(CharBuffer.wrap(chars)).array();
-                check(Arrays.equals(bytes2, bytes));
-            } catch (Throwable t) {
-                System.out.printf("cs=%s c=%02x%n", cs, c);
-                unexpected(t);
+                char[] chars = decoder.decode(ByteBuffer.wrap(bytes)).array();
+                byte[] bytes2 = encoder.encode(cbFunction.apply(new String(chars))).array();
+                assertArrayEquals(bytes, bytes2);
+            } catch (Exception e) {
+                // ignore unmappable characters
             }
         }
     }
-
-    private static void realMain(String[] args) throws Throwable {
-        for (Map.Entry<String,Charset> e
-                 : Charset.availableCharsets().entrySet()) {
-            String csn = e.getKey();
-            Charset cs = e.getValue();
-            if (csn.matches(".*(8859).*"))
-                try { testCharset(cs); }
-                catch (Throwable t) { unexpected(t); }
-        }
-    }
-
-    //--------------------- Infrastructure ---------------------------
-    static volatile int passed = 0, failed = 0;
-    static void pass() {passed++;}
-    static void fail() {failed++; Thread.dumpStack();}
-    static void fail(String msg) {System.out.println(msg); fail();}
-    static void unexpected(Throwable t) {failed++; t.printStackTrace();}
-    static void check(boolean cond) {if (cond) pass(); else fail();}
-    static void equal(Object x, Object y) {
-        if (x == null ? y == null : x.equals(y)) pass();
-        else fail(x + " not equal to " + y);}
-    public static void main(String[] args) throws Throwable {
-        try {realMain(args);} catch (Throwable t) {unexpected(t);}
-        System.out.printf("%nPassed = %d, failed = %d%n%n", passed, failed);
-        if (failed > 0) throw new AssertionError("Some tests failed");}
 }
